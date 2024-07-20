b8cc2635fe4c66b9208a5469b6319c52bbd79cb6
Yuuya Okatani <yuuyaokatani@gmail.com>
2024-07-20 17:22:43 -0300

    closes #2 - entidade pronta

---
+++ project-backend/src/main/java/com/bluemango/project_backend/models/Seller.java
@@ -0,0 +1,91 @@
+package com.bluemango.project_backend.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TBL_SELLER")
+public class Seller {
+    private Long id; 
+    private String name;
+    private Double salary; 
+    private Double bonus; 
+    private Integer gender;
+
+    public Seller() {}
+
+    public Seller(Long id, String name, Double salary, Double bonus, Integer gender) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.bonus = bonus;
+        this.gender = gender;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Double getSalary() {
+        return salary;
+    }
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+    public Double getBonus() {
+        return bonus;
+    }
+    public void setBonus(Double bonus) {
+        this.bonus = bonus;
+    }
+    public Integer getGender() {
+        return gender;
+    }
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Seller other = (Seller) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Seller [id=" + id + ", name=" + name + ", salary=" + salary + ", bonus=" + bonus + ", gender=" + gender
+                + "]";
+    } 
+
+    
+    
+
+    
+}
--- project-backend/src/main/java/com/bluemango/project_backend/resources/SellerController.java
+++ project-backend/src/main/java/com/bluemango/project_backend/resources/SellerController.java
@@ -1,20 +1,14 @@
 package com.bluemango.project_backend.resources;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
+@RequestMapping("sellers")
+public class SellerController {
 
-public class helloController {
-
-    @GetMapping("/hello")
-    public String helloWorld() {
-        return "Hello, World!";
-    }
-
-
-
+    
     
 }
